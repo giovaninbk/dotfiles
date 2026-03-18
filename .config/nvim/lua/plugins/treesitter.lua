5f7a31464ada3a3c2d43af8c99c415ab0d5ab918
Giovani Granero <giovani.granero@nubank.com.br>
2026-03-18 15:32:16 -0300

    Fixing treesitter

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,9 +1,10 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
+	branch = "main",
 	event = { "BufReadPost", "BufNewFile" },
 	cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
-	build = ":TSUpdate | TSInstallAll",
-	main = "nvim-treesitter.configs",
+	build = ":TSUpdate",
+	main = "nvim-treesitter.config",
 	opts = {
 		auto_install = true,
 		highlight = { enable = true },
