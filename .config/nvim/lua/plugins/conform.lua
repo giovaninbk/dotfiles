97160a12c6c8d2240d0f6b946b9538aaabc979f8
Giovani Granero <giovani.granero@nubank.com.br>
2026-03-19 16:52:02 -0300

    Fixing cljfmt

--- .config/nvim/lua/plugins/conform.lua
+++ .config/nvim/lua/plugins/conform.lua
@@ -2,17 +2,16 @@ return {
 	"stevearc/conform.nvim",
 	event = "BufWritePre",
 	opts = {
-		formatters_by_ft = {
+formatters_by_ft = {
 			lua = { "stylua" },
-			go = { "gofumpt", "goimports-reviser", "golines" },
-			clj = { "cljfmt" },
+			clojure = { "cljfmt" },
 			-- css = { "prettier" },
 			-- html = { "prettier" },
 		},
-		format_on_save = {
-			-- These options will be passed to conform.format()
-			timeout_ms = 500,
-			lsp_fallback = true,
-		},
+		format_on_save = function(bufnr)
+			-- Disable lsp fallback for clojure — cljfmt handles it via conform
+			local lsp_fallback = vim.bo[bufnr].filetype ~= "clojure"
+			return { timeout_ms = 500, lsp_fallback = lsp_fallback }
+		end,
 	},
 }
